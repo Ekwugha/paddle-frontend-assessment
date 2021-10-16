f4b8c7fc63f5233b87e7450afb1a15527399ac87
Ekwugha <elloekwugha@gmail.com>
2021-10-16 17:28:17 +0100

    features component added

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Landingpage from '../../components/landingpage/landingpage.component';
 import User from '../../components/users/users.component';
+import Features from '../../components/features/features.component';
 
 function HomePage() {
     return (
         <div>
             <Landingpage />
             <User />
+            <Features />
         </div>
     )
 }
